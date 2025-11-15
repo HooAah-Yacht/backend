449a74f3284919ac29f930422acf6a049ccf5217
김도현 KimDohyun <163822282+pu2rile@users.noreply.github.com>
2025-11-15 16:33:31 +0900

    refactor (calendar): part relation

--- src/main/java/HooYah/Yacht/calendar/domain/Calendar.java
+++ src/main/java/HooYah/Yacht/calendar/domain/Calendar.java
@@ -1,9 +1,13 @@
 package HooYah.Yacht.calendar.domain;
 
+import HooYah.Yacht.part.domain.Part;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.OffsetDateTime;
 import lombok.AccessLevel;
@@ -21,7 +25,9 @@ public class Calendar {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long partId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "part_id")
+    private Part part;
 
     private OffsetDateTime startDate;
 
@@ -30,15 +36,15 @@ public class Calendar {
     private String content;
 
     @Builder
-    private Calendar(Long partId, OffsetDateTime startDate, OffsetDateTime endDate, String content) {
-        this.partId = partId;
+    private Calendar(Part part, OffsetDateTime startDate, OffsetDateTime endDate, String content) {
+        this.part = part;
         this.startDate = startDate;
         this.endDate = endDate;
         this.content = content;
     }
 
-    public void update(Long partId, OffsetDateTime startDate, OffsetDateTime endDate, String content) {
-        this.partId = partId;
+    public void update(Part part, OffsetDateTime startDate, OffsetDateTime endDate, String content) {
+        this.part = part;
         this.startDate = startDate;
         this.endDate = endDate;
         this.content = content;
