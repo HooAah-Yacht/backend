d4fc7bf8d779b213f9fbab9ab88bbaee06473e8d
김도현 KimDohyun <163822282+pu2rile@users.noreply.github.com>
2025-11-15 17:30:41 +0900

    feat (dto): add type, yacht, completed

--- src/main/java/HooYah/Yacht/calendar/dto/response/CalendarInfo.java
+++ src/main/java/HooYah/Yacht/calendar/dto/response/CalendarInfo.java
@@ -1,6 +1,7 @@
 package HooYah.Yacht.calendar.dto.response;
 
 import HooYah.Yacht.calendar.domain.Calendar;
+import HooYah.Yacht.calendar.domain.CalendarType;
 import java.time.OffsetDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,17 +13,23 @@ import lombok.Getter;
 public class CalendarInfo {
 
     private Long id;
+    private CalendarType type;
     private Long partId;
+    private Long yachtId;
     private OffsetDateTime startDate;
     private OffsetDateTime endDate;
+    private boolean completed;
     private String content;
 
     public static CalendarInfo from(Calendar calendar) {
         return CalendarInfo.builder()
                 .id(calendar.getId())
+                .type(calendar.getType())
                 .partId(calendar.getPart() != null ? calendar.getPart().getId() : null)
+                .yachtId(calendar.getYacht() != null ? calendar.getYacht().getId() : null)
                 .startDate(calendar.getStartDate())
                 .endDate(calendar.getEndDate())
+                .completed(calendar.isCompleted())
                 .content(calendar.getContent())
                 .build();
     }
